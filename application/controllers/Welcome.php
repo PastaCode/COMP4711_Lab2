3c69735311454be2519f8a883f3093866464bcb5
Jeremy Yang <yy25036542@gmail.com>
2016-09-16 21:15:02 -0700

    Rollback Signed-off-by:Jeremy Yang <yy25036542@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -23,8 +23,7 @@ class Welcome extends Application
 		$authors = array ();
 		foreach ($source as $record)
 		{
-			$authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where'], 
-                                                'what' => $record['what']);
+			$authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where']);
 		}
 		$this->data['authors'] = $authors;
 
